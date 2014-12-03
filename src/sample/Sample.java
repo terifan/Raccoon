6ca501fb81f542620226a6a0c6cd69fc9ff23cf1
Patrik Olsson <patrik.olsson@surikat.se>
2014-12-03 10:01:06 +0100

    a test commit

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -6,19 +6,19 @@ import org.terifan.v1.raccoon.Key;
 import org.terifan.v1.raccoon.OpenOption;
 import org.terifan.v1.util.log.Log;
 
-public class Sample 
+public class Sample
 {
 	public static void main(String... args)
 	{
 		try
 		{
-			try (Database db = Database.open(new File("d:/log.db"), OpenOption.CREATE_NEW))
+			try (Database db = Database.open(new File("d:/sample.db"), OpenOption.CREATE_NEW))
 			{
 				db.save(new Fruit("apple", 52));
 				db.commit();
 			}
 
-			try (Database db = Database.open(new File("d:/log.db"), OpenOption.OPEN))
+			try (Database db = Database.open(new File("d:/sample.db"), OpenOption.OPEN))
 			{
 				db.list(Fruit.class).stream().forEach(Log.out::println);
 			}
@@ -28,8 +28,8 @@ public class Sample
 			e.printStackTrace(System.out);
 		}
 	}
-	
-	
+
+
 	static class Fruit
 	{
 		@Key String name;
