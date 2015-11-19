75951024acef29350ab01f6a6b50ab1869851b30
terifan <pinjata69@gmail.com>
2015-11-19 08:50:28 +0100

    updated

--- src/tests/ConcurrencyTest.java
+++ src/tests/ConcurrencyTest.java
@@ -1,124 +1,124 @@
 package tests;
 
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.terifan.raccoon.Database;
 import org.terifan.raccoon.OpenOption;
-import org.terifan.raccoon.io.AccessCredentials;
 import org.terifan.raccoon.io.MemoryBlockDevice;
 import org.terifan.raccoon.util.Log;
 
 
-public class ConcurrencyTest 
+public class ConcurrencyTest
 {
+	private static Database db;
+	private static String[] keys;
+
+
 	public static void main(String... args)
 	{
 		try
 		{
 			Random rnd = new Random();
-			String[] keys = new String[100000];
+
+			keys = new String[10000];
 			char[] buf = new char[40];
 			for (int i = 0; i < keys.length; i++)
 			{
 				for (int j = 0; j < 40; j++)
 				{
-					buf[j] = "abcdefghijklmnopqrstuvwxyz".charAt(rnd.nextInt(26));
+					buf[j] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-/".charAt(rnd.nextInt(64));
 				}
 				keys[i] = new String(buf, 0, 5 + rnd.nextInt(35));
 			}
 
-			AccessCredentials accessCredentials = new AccessCredentials("password");
-
 			MemoryBlockDevice blockDevice = new MemoryBlockDevice(4096);
 
-			try (Database db = Database.open(blockDevice, OpenOption.CREATE_NEW, accessCredentials))
+			db = Database.open(blockDevice, OpenOption.CREATE_NEW);
+
+			ArrayList<Reader> readers = new ArrayList<>();
+			for (int i = 0; i < 6; i++)
 			{
-				try (__FixedThreadExecutor executor2 = new __FixedThreadExecutor(4))
+				Reader reader = new Reader();
+				readers.add(reader);
+				reader.start();
+			}
+
+			try (__FixedThreadExecutor executor = new __FixedThreadExecutor(2))
+			{
+				for (int k = 0; k < 10; k++)
 				{
-					try (__FixedThreadExecutor executor = new __FixedThreadExecutor(4))
+					for (int i = 0; i < keys.length; i++)
 					{
-						final AtomicInteger in = new AtomicInteger();
-
-						for (int i = 0; i < keys.length; i++)
+						final int j = i;
+						executor.submit(()->
 						{
-							executor.submit(()->
+							try
 							{
-								try
+								db.save(new _Fruit1K(keys[j], 52.12));
+							}
+							catch (Exception e)
+							{
+								synchronized (ConcurrencyTest.class)
 								{
-									final String key = keys[in.getAndIncrement()];
-									db.save(new _Fruit1K(key, 52.12));
-
-									executor2.submit(()->
-									{
-										try
-										{
-											if (!db.get(new _Fruit1K(key)))
-											{
-												synchronized (ConcurrencyTest.class)
-												{
-													Log.out.println("err");
-												}
-											}
-										}
-										catch (Exception e)
-										{
-											synchronized (ConcurrencyTest.class)
-											{
-												e.printStackTrace(Log.out);
-											}
-										}
-									});
+									e.printStackTrace(Log.out);
 								}
-								catch (Exception e)
-								{
-									synchronized (ConcurrencyTest.class)
-									{
-										e.printStackTrace(Log.out);
-									}
-								}
-							});
-						}
+							}
+						});
 					}
 				}
+			}
 
-//				db.commit();
-//
-//				for (;;){
-//					Log.out.println("#");
-//				try (__FixedThreadExecutor executor = new __FixedThreadExecutor(8))
-//				{
-//					final AtomicInteger in = new AtomicInteger();
-//
-//					for (int i = 0; i < 10000; i++)
-//					{
-//						executor.submit(()->
-//						{
-//							try
-//							{
-//								final String key = "apple" + in.incrementAndGet();
-//								if (!db.get(new _Fruit1K(key)))
-//								{
-//									synchronized (ConcurrencyTest.class)
-//									{
-//										Log.out.println("err");
-//									}
-//								}
-//							}
-//							catch (Exception e)
-//							{
-//								synchronized (ConcurrencyTest.class)
-//								{
-//									e.printStackTrace(Log.out);
-//								}
-//							}
-//						});
-//					}
-//				}}
+			for (String key : keys)
+			{
+				if (!db.get(new _Fruit1K(key)))
+				{
+					synchronized (ConcurrencyTest.class)
+					{
+						Log.out.println("err");
+					}
+				}
+			}
+
+			Log.out.println("#");
+
+			for (Reader reader : readers)
+			{
+				reader.stop = true;
+				Log.out.println(reader.count);
 			}
 		}
 		catch (Throwable e)
 		{
 			e.printStackTrace(System.out);
+		}
+	}
+
+
+	private static class Reader extends Thread
+	{
+		boolean stop;
+		int count;
+
+		@Override
+		public void run()
+		{
+			Random rnd = new Random();
+
+			while (!stop)
+			{
+				try
+				{
+					db.get(new _Fruit1K(keys[rnd.nextInt(keys.length)]));
+					count++;
+				}
+				catch (Exception e)
+				{
+					synchronized (ConcurrencyTest.class)
+					{
+						e.printStackTrace(Log.out);
+					}
+				}
+			}
 		}
 	}
 }
