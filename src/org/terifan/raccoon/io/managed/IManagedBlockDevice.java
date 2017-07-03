b516cda345f3ea5d452033e18b6a814a7d62fb5b
terifan <pinjata69@gmail.com>
2017-07-03 13:56:59 +0200

    update

--- src/org/terifan/raccoon/io/managed/IManagedBlockDevice.java
+++ src/org/terifan/raccoon/io/managed/IManagedBlockDevice.java
@@ -1,93 +1,12 @@
 package org.terifan.raccoon.io.managed;
 
 import java.io.IOException;
+import org.terifan.raccoon.io.IBlockDevice;
 
 
-public interface IManagedBlockDevice extends AutoCloseable //IPhysicalBlockDevice
+public interface IManagedBlockDevice extends IBlockDevice
 {
 	final static int EXTRA_DATA_LIMIT = 64 + 256;
-
-	/**
-	 * Read a single block from the device.
-	 *
-	 * @param aBlockIndex
-	 *   the index of the block.
-	 * @param aBuffer
-	 *   destination array for block the be read
-	 * @param aBufferOffset
-	 *   offset in the block array where block data is stored
-	 * @param aBufferLength
-	 *   length of buffer to write, must be dividable by block size
-	 * @param aIV
-	 *   128 bit seed value that may be used by block device implementations performing cryptography
-	 */
-	void readBlock(long aBlockIndex, byte[] aBuffer, int aBufferOffset, int aBufferLength, long[] aIV) throws IOException;
-
-
-	/**
-	 * Write a single block to the device.
-	 *
-	 * @param aBlockIndex
-	 *   the index of the block.
-	 * @param aBuffer
-	 *   data to be written to the device
-	 * @param aBufferOffset
-	 *   offset in the block array where block data is stored
-	 * @param aBufferLength
-	 *   length of buffer to write, must be dividable by block size
-	 * @param aIV
-	 *   128 bit seed value that may be used by block device implementations performing cryptography
-	 */
-	void writeBlock(long aBlockIndex, byte[] aBuffer, int aBufferOffset, int aBufferLength, long[] aIV) throws IOException;
-
-
-	/**
-	 * Attempt to flush any changes made to blocks
-	 *
-	 * @param aMetadata
-	 *   force update of metadata
-	 */
-	void commit(boolean aMetadata) throws IOException;
-
-
-	/**
-	 * Close the block device. Information not previously committed will be lost.
-	 */
-	@Override
-	void close() throws IOException;
-
-
-	/**
-	 * Return number of blocks available on this device.
-	 */
-	long length() throws IOException;
-
-
-	/**
-	 * @return
-	 *   the size of each block on this device.
-	 */
-	int getBlockSize() throws IOException;
-
-
-	/**
-	 * Truncates this block device to the number of blocks specified.
-	 *
-	 * @param aNewLength
-	 *   number of blocks
-	 */
-	void setLength(long aNewLength) throws IOException;
-
-
-	/**
-	 * Close the block device not permitting any future changes to happen. Invoked when an error has occurred that may jeopardize the integrity of the block device.
-	 *
-	 * Default implementation calls close.
-	 */
-	default void forceClose() throws IOException
-	{
-		close();
-	}
 
 
 	/**
@@ -95,24 +14,6 @@ public interface IManagedBlockDevice extends AutoCloseable //IPhysicalBlockDevic
 	 *   true if the block device has pending changes requiring a commit.
 	 */
 	boolean isModified();
-
-
-//	/**
-//	 * @return
-//	 *   the "extra data" block.
-//	 */
-//	byte[] getExtraData();
-//
-//
-//	/**
-//	 * Sets the "extra data" block of this block device.
-//	 *
-//	 * The block device must be able to store an "extra data" block with minimum 256 bytes. The block I/O must be transactional safe and the block is written as the block device changes are committed.
-//	 *
-//	 * @param aExtra
-//	 *   the extra data.
-//	 */
-//	void setExtraData(byte[] aExtra);
 
 
 	/**
