15f1d24b7934dfa69681285fbd295eea6309554a
terifan <pinjata69@gmail.com>
2023-10-20 16:34:41 +0200

    update

---
+++ src/org/terifan/raccoon/RaccoonHeap.java
@@ -0,0 +1,62 @@
+package org.terifan.raccoon;
+
+import java.util.function.Consumer;
+import org.terifan.raccoon.document.Document;
+
+// 128 byte =
+//	80 byte ptr
+//	32 byte meta
+//		document / lob
+//	16 byte hash of _id
+
+public class RaccoonHeap
+{
+//	RaccoonHeap(LobByteChannel aChannel, int aRecordSize)
+//	{
+//	}
+//
+//
+//	public long insert(Document aDocument)
+//	{
+//	}
+//
+//
+//	public RaccoonHeap put(long aId, Document aDocument)
+//	{
+//	}
+//
+//
+//	public Document get(long aId)
+//	{
+//	}
+//
+//
+//	public Document delete(long aId)
+//	{
+//	}
+//
+//
+//	public boolean exists(long aId)
+//	{
+//	}
+//
+//
+//	public List<Document> query(Document aQuery)
+//	{
+//	}
+//
+//
+//	public void query(Document aQuery, Consumer<Document> aConsumer)
+//	{
+//	}
+//
+//
+//	public void forEach(Consumer<Document> aConsumer)
+//	{
+//	}
+//
+//
+//	public long size()
+//	{
+//	}
+}
